23d4ca39fa119ad68d742ab500d2f02e321fe9e6
boris lazarev <blazarev@BLAZAREV2-MAC.local>
2023-03-27 11:07:01 +0200

    Updates

---
+++ 902.f16fc7a267b6cd1dd1c5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmf_products=self.webpackChunkmf_products||[]).push([[902],{21902:(e,t,c)=>{c.r(t),c.d(t,{mount:()=>u});var d=c(95552),r=c.n(d);const u=e=>{let t="";for(let e=0;e<5;e++)t+=`<div>${r().commerce.productName()}</div>`;e.innerHTML=t},n=document.getElementById("dev-products");n&&u(n)}}]);
